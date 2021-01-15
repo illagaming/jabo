ea00e6d4437a0bd082dd4f53cf00a872f1ad051d
illagaming <arnaudpaul15@gmail.com>
2021-01-15 12:23:52 +0100

    Ajout mode night

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
 
-//if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-//	alert('Je suis en dark mode')
-//}
-
-
+/*if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+	alert('Je suis en dark mode')
+}
+*/
 const btnToggle = document.querySelector('.btn-toggle');
+
+let img = document.getElementById('img3')
+let img2 = document.getElementById('img2')
 
 btnToggle.addEventListener('click', () => {
 
-	const body = document.body;
+	var body = document.body;
 
 	if(body.classList.contains('dark')){
 
 		body.classList.add('light')
 		body.classList.remove('dark')
+		img.src = 'img/Noir_petit.png';
+		img2.src = 'img/Noir_petit.png';
 		btnToggle.innerHTML = "🌙"
 
 	} else if(body.classList.contains('light')){
 
 		body.classList.add('dark')
 		body.classList.remove('light')
+		img.src = 'img/Blanc_petit.png';
+		img2.src = 'img/Blanc_petit.png';
 		btnToggle.innerHTML = "☀️"
 	}
 })
-
